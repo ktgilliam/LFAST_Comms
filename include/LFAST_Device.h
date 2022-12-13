5f65355848a6e83775239088f06391a0382aa144
Kevin Gilliam <keving@arizona.edu>
2022-12-13 14:50:02 -0700

    added device name field to LFAST_Device

--- include/LFAST_Device.h
+++ include/LFAST_Device.h
@@ -7,11 +7,12 @@
 class LFAST_Device
 {
 private:
+    char DeviceName[30];
 protected:
     static TerminalInterface *cli;
     virtual void setupPersistentFields() = 0;
 
 public:
-    virtual void connectTerminalInterface(TerminalInterface *_cli);
+    virtual void connectTerminalInterface(TerminalInterface *_cli, const char* dev='');
     virtual void serviceCLI() {};
 };
--- src/LFAST_Device.cc
+++ src/LFAST_Device.cc
@@ -1,10 +1,12 @@
 #include "LFAST_Device.h"
+#include <cstring>
 
 TerminalInterface* LFAST_Device::cli = nullptr;
 
-void LFAST_Device::connectTerminalInterface(TerminalInterface *_cli)
+void LFAST_Device::connectTerminalInterface(TerminalInterface *_cli, const char* dev)
 {
     cli = _cli;
+    std::strcpy(DeviceName, dev);
     setupPersistentFields();
     cli->printPersistentFieldLabels();
 }
